7040ebe8027d9cc69c0df3e154a53077e4daf493
Sam Kiki Baker <samkikibaker@hotmail.co.uk>
2025-01-04 19:24:07 +0000

    Keep empty cache, songs and playlists dirs

--- run.sh
+++ run.sh
@@ -6,4 +6,8 @@ docker build -t song_categoriser .
 
 # Step 2: Run the Docker container
 echo "Running Docker container..."
-docker run --shm-size=14g --cpus=8 --mount type=bind,source=$(pwd)/songs,target=/app/songs --mount type=bind,source=$(pwd)/cache,target=/app/cache -p 8501:8501 song_categoriser
+docker run --shm-size=14g --cpus=8 --mount type=bind,source=$(pwd)/songs,target=/app/songs --mount type=bind,source=$(pwd)/cache,target=/app/cache -p 8501:8501 song_categoriser &
+
+# Step 3: Open the Streamlit app in the browser automatically
+echo "Opening Streamlit app in the browser..."
+xdg-open http://localhost:8501
